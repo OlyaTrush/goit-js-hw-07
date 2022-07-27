215fe61a7b036a669d3fd4d5c0ecd6e2abf4b067
OlyaTrush <olya.trush@gmail.com>
2022-07-27 18:35:25 +0300

    add tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,77 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
+//  Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
+// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач
+//  буде перенаправлений на іншу сторінку.
+// Заборони цю поведінку за замовчуванням.
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури
+// було тільки доти, доки відкрите модальне вікно.
+//  Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
 
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createGalleryCollection(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryCollection(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(" ");
+}
 console.log(galleryItems);
+
+galleryContainer.addEventListener("click", onImgClick);
+
+let modalImg;
+
+function onImgClick(e) {
+
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  modalImg = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}" width="800" height="600">
+ `);
+
+  modalImg.show();
+
+  modalClose();
+
+}
+
+function modalClose () {
+  if (modalImg.visible()) {
+    window.addEventListener("keydown", onCloseKeyEsc);
+  }
+
+  function onCloseKeyEsc(e) {
+    if (e.code === "Escape") {
+      modalImg.close();
+      window.removeEventListener("keydown", onCloseKeyEsc);
+      return;
+    }
+  }
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
+// Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. 
+// Використовуй готовий код з першого завдання.
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: 
+// simple-lightbox.min.js і simple-lightbox.min.css.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. 
+// Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
+//  Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+
+
+
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createGalleryCollection(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryCollection(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join(" ");
+}
 
 console.log(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay: 250});
+
+console.log(captionDelay);
+function BlockEventClickLoad(e) {
+  e.preventDefault();
+}
