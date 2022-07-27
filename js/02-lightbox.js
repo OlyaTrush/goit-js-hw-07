c25dda8510f8fcea063122750f919352df96f4a6
OlyaTrush <olya.trush@gmail.com>
2022-07-27 19:59:50 +0300

    formatting

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,19 +1,15 @@
 // Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. 
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 // Використовуй готовий код з першого завдання.
-// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: 
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли:
 // simple-lightbox.min.js і simple-lightbox.min.css.
-// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. 
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery.
 // Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
 // Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
 //  Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
 
-
-
-
-
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".gallery");
 const imageMarkup = createGalleryCollection(galleryItems);
@@ -32,7 +28,10 @@ function createGalleryCollection(galleryItems) {
 
 console.log(galleryItems);
 
-let lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay: 250});
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(captionDelay);
 function BlockEventClickLoad(e) {
